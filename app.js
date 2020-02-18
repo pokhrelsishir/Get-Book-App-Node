5af1c785c06a5c2b67e3f2e0451ade2500cf7a3a
Sishir Pokhrel <pokhrelsishir@gmail.com>
2020-02-18 00:47:12 -0600

    Adding nessary http methods

--- app.js
+++ app.js
@@ -2,23 +2,57 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI');
+//const db = mongoose.connect("mongodb://localhost:4000/bookAPI", { useNewUrlParser: true });
+
+const db = mongoose.connect('mongodb+srv: {mongodb://localhost:4000/bookAPI}', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 
 
 bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+  })
   .get((req, res) => {
-    const response = {
-      hello: 'This is my API !!!'
-    };
-    res.json(response);
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
 
-    //res.send(response);
-    //res.render(response);
-  }
-  );
+    });
+  });
+
+
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+
+
+    Book.findById(err.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+
+    });
+  });
+
+// const response = {
+//   hello: 'This is my API !!!'
+// };
+// res.json(response);
+
+//res.send(response);
+//res.render(response);
 
 app.use('/api', bookRouter);
 
